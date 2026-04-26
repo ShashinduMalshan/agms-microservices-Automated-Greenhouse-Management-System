29be3d31d716564e25d8e458ab466d70fd4f1f40
shasidumalshan <shasidumalshan9579@gmail.com>
2026-04-26 18:03:13 +0530

    feat(crop-service): implement crop controller and service configuration

---
+++ crop-inventory-service/src/test/java/lk/ijse/cropinventory/CropInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.cropinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
